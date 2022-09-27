578a0af67d0d8ad835c5260f523fb48ebf5c60cc
DavidTG7 <davidthekoa@gmail.com>
2022-09-27 17:09:50 -0500

    Update package.json file, create test folder, create first test for a component

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Footer from '../../components/Footer';
+
+describe('<Footer />', () => {
+  test('Footer Rendering', () => {
+    const footer = mount(<Footer />);
+    expect(footer.length).toEqual(1);
+  });
+});
