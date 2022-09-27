aa3e612449c326d75c47cc54f92c1918fdd4d7bd
DavidTG7 <davidthekoa@gmail.com>
2022-09-27 17:35:58 -0500

    Create tests to Footer content: title and copy rights

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,16 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  const footer = mount(<Footer />);
   test('Footer Rendering', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+
+  test('Title render', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+
+  test('Copy lefts xd', () => {
+    expect(footer.find('.Footer-copy').text()).toEqual('Todos los Izquierdos Reservados');
   });
 });
